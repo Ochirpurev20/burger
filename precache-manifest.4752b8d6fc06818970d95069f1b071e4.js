b5f85b7a4dc859ab6424f763f82daaf15c610882
Ochirpurev20 <oscaravna2020@gmail.com>
2020-08-19 16:52:51 +0800

    Updates

---
+++ precache-manifest.4752b8d6fc06818970d95069f1b071e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d02985d039764ea61a0dd4fbeb0fd0",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "0b6e3c5fc6f5649c6846",
+    "url": "/burger/static/css/main.fc85ad1c.chunk.css"
+  },
+  {
+    "revision": "597cf5d15b6e4b1cd67d",
+    "url": "/burger/static/js/2.f3b35900.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/burger/static/js/2.f3b35900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6e3c5fc6f5649c6846",
+    "url": "/burger/static/js/main.1c1c68a9.chunk.js"
+  },
+  {
+    "revision": "75ab14ff4938c2d16ae9",
+    "url": "/burger/static/js/runtime-main.a489e444.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
